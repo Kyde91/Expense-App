c8bffff9cf10ecc96fe49a2eaea43767f93b8451
Kyde <kmc91@hotmail.com>
2024-01-11 17:39:48 +0000

    added expense function

--- script.js
+++ script.js
@@ -16,13 +16,21 @@ function displayDetails() {
   var display = document.getElementById("display");
 
   var newRow = display.insertRow(row);
+
   var cell1 = newRow.insertCell(0);
   var cell2 = newRow.insertCell(1);
   var cell3 = newRow.insertCell(2);
+  var cell4 = newRow.insertCell(3);
 
   cell1.innerHTML = item;
   cell2.innerHTML = date;
   cell3.innerHTML = cost;
+  cell4.innerHTML = "<button>X</button>";
+
+  cell4.addEventListener("click", (e) => {
+    document.getElementById("display").deleteRow(-1);
+    row--;
+  });
 
   row++;
 }
